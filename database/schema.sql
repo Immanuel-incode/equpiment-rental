18eaec9c35f4945415ef2e0633100b9a3a882e18
Emmanuel Onohaebi <onohaebii@gmail.com>
2026-02-04 12:39:52 +0000

    Schema database update

--- database/schema.sql
+++ database/schema.sql
@@ -4,26 +4,30 @@ CREATE TABLE users (
   email VARCHAR(150) NOT NULL UNIQUE,
   password_hash VARCHAR(255) NOT NULL,
   role ENUM('admin','user') NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE equipment (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(150) NOT NULL,
-  description TEXT,
-  quantity_available INT NOT NULL CHECK (quantity_available >= 0),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  name VARCHAR(150),
+  total-_quantity INT NOT NULL,
+  quantity_available INT NOT NULL CHECK,
 );
 
 CREATE TABLE rentals (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
-  equipment_id INT NOT NULL,
-  qty INT NOT NULL CHECK (qty > 0),
   rented_at DATETIME NOT NULL,
   due_at DATETIME NOT NULL,
   returned_at DATETIME NULL,
   status ENUM('rented','returned','overdue') NOT NULL DEFAULT 'rented',
   FOREIGN KEY (user_id) REFERENCES users(id),
+);
+
+CREATE TABLE rental_items (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  rental_id INT NOT NULL,
+  equipment_id INT NOT NULL,
+  quantity INT NOT NULL,
+  FOREIGN KEY (rental_id) REFERENCES rentals(id),
   FOREIGN KEY (equipment_id) REFERENCES equipment(id)
 );
